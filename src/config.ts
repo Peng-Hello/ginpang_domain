eb80970a58796a61dc940b055624707a6117262d
Peng <70511773+Peng-Hello@users.noreply.github.com>
2024-11-02 14:36:46 +0800

    feat: ✨ 配置评论系统

--- src/config.ts
+++ src/config.ts
@@ -21,10 +21,10 @@ export const Socials: Record<string, Record<string, string>> = {
 // doc: https://giscus.app
 // data-theme is auto changed between noborder_light / noborder_gray
 export const GiscusConfig: Record<string, string> = {
-	'data-repo': 'ladit/astro-blog-zozo',
-	'data-repo-id': 'R_kgDOLgobXQ',
+	'data-repo': 'Peng-Hello/ginpang_domain',
+	'data-repo-id': 'R_kgDONJROmA',
 	'data-category': 'Announcements',
-	'data-category-id': 'DIC_kwDOLgobXc4Cd_N6',
+	'data-category-id': 'DIC_kwDONJROmM4Cj6PO',
 	'data-mapping': 'pathname',
 	'data-strict': '0',
 	'data-reactions-enabled': '1',
